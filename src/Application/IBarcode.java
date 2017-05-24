3647924fb7c80017759e9a152f562099cd7b594b
Felix Hillebrand <97hilfel@gmail.com>
2017-05-24 16:37:29 +0200

    Added IBarcode interface

---
+++ src/Application/IBarcode.java
@@ -0,0 +1,25 @@
+package Application;
+
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+
+/**
+ * Interface to be implemented by all barcodes.
+ */
+public interface IBarcode {
+	
+	
+	/**
+	 * runs the current barcode generator
+	 * @return generated barcode as Image
+	 */
+	public Image runGenerator() throws Exception;
+	
+	/**
+	 * gets the properties mandatory to be implemented
+	 * @return A anchorPane that contains controls which set the properties
+	 * (examples in EAN13 & EAN8 class)
+	 */
+	public Node mandatoryProperties();
+	
+}
