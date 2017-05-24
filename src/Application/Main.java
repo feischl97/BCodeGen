c08c553fb90dfbbe39f4c61dfbad0a3103ed280c
Felix Hillebrand <97hilfel@gmail.com>
2017-05-24 16:34:50 +0200

    finished feature event system still pending

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -4,16 +4,38 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Controller guiCtrl;
+    
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        primaryStage.setTitle("Barcode Generator");
-        primaryStage.setScene(new Scene(root, 1080, 720));
-        primaryStage.show();
+        
+        try {
+    
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
+            
+            Parent root = fxmlLoader.load();
+            
+            guiCtrl = fxmlLoader.getController();
+            
+            primaryStage.setTitle("Barcode Generator");
+            primaryStage.setScene(new Scene(root, 1080, 720));
+            primaryStage.show();
+            
+            guiCtrl.setup();
+        }
+        catch(Exception e){
+    
+            Alert eAlert = new Alert(Alert.AlertType.ERROR);
+            eAlert.setTitle("Not able to load FXML");
+            eAlert.setHeaderText("An exception occured when attempting to load \"MainWindow.fxml\" resource");
+            eAlert.setContentText(e.toString());
+            eAlert.showAndWait();
+        }
     }
 
     public static void main(String[] args) {
